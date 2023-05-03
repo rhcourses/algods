cf8b18148a60c2d150ceba0cd7fa3cb60aacfa24
Reiner Hüchting <dev@reinerhuechting.de>
2023-05-03 15:03:15 +0200

    Add code examples for insertion sort.

---
+++ examples/sorting/insertionsort.go
@@ -0,0 +1,8 @@
+package sorting
+
+// InsertionSort sorts the given list using the insertion sort algorithm.
+func InsertionSort(list []int) {
+	for i := 1; i < len(list); i++ {
+		MoveLeft(list, i)
+	}
+}
