b3462ff6dd50967b9e9f61ff92d00224b4679c93
Reiner Hüchting <dev@reinerhuechting.de>
2023-05-03 16:27:01 +0200

    Add an implementation of a merge operation for mergesort.

---
+++ examples/sorting/merge_test.go
@@ -0,0 +1,11 @@
+package sorting
+
+import "testing"
+
+func TestMerge(t *testing.T) {
+	list1 := []int{1, 3, 5, 7, 9}
+	list2 := []int{2, 4, 6, 8, 10}
+
+	list := Merge(list1, list2)
+	AssertListsEqual(t, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, list)
+}
